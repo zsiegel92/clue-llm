30cf2fab2f08d4eab4fff8ad4e0e0050d8296466
zsiegel92 <zsiegel92@gmail.com>
2026-01-10 00:24:16 -0500

    schema

--- lib/clue-schema.ts
+++ lib/clue-schema.ts
@@ -4,6 +4,8 @@ import {
   places,
   companies,
   institutions,
+  foods,
+  materials,
 } from "./single-token-strings";
 
 export const scenarioSchema = z.object({
@@ -11,6 +13,8 @@ export const scenarioSchema = z.object({
   place: z.enum(places),
   company: z.enum(companies),
   institution: z.enum(institutions),
+  food: z.enum(foods),
+  material: z.enum(materials),
 });
 
 export type Scenario = z.infer<typeof scenarioSchema>;
@@ -18,3 +22,5 @@ export type Technology = Scenario["technology"];
 export type Place = Scenario["place"];
 export type Company = Scenario["company"];
 export type Institution = Scenario["institution"];
+export type Food = Scenario["food"];
+export type Material = Scenario["material"];
