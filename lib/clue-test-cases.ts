d8dd142986bfa1fea3c754f2418b0ca7ba7f64aa
zsiegel92 <zsiegel92@gmail.com>
2026-02-05 20:27:44 -0500

    load fewshot/validate

--- lib/clue-test-cases.ts
+++ lib/clue-test-cases.ts
@@ -1,8 +1,13 @@
 import data from "./clue-test-cases.json";
+import fewShotData from "./few-shot-example-clue-test-cases.json";
 import { type ClueTestCases, clueTestCasesSchema } from "./schemas";
 
 export const clueTestCases = clueTestCasesSchema.parse(
   data,
+) satisfies ClueTestCases;
+
+export const fewShotExamples = clueTestCasesSchema.parse(
+  fewShotData,
 ) satisfies ClueTestCases;
 
 export type {
